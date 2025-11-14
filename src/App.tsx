30d024fac48aebc8975c02a555fc8888c3955d58
EmanSan9 <sanna950@gmail.com>
2025-11-14 18:48:14 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900">
+      <h1 className="rounded-lg bg-blue-600 p-4 text-3xl font-bold text-white shadow-lg">
+        Se vedi questo, Tailwind funziona!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- Dice a v4 cosa scansionare
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
